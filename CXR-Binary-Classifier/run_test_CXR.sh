87b10fc13559b17a61fe844d7602e315832eef87
Manav Gakhar <55736716+manavgakhar@users.noreply.github.com>
2021-02-12 15:30:20 +0530

    Update run_test_CXR.sh

--- CXR-Binary-Classifier/run_test_CXR.sh
+++ CXR-Binary-Classifier/run_test_CXR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python test_densenet.py --img_size 256 --crop_size 224 --batch_size 64 --learning_rate 0.001 --gpu 0 --test_labels 'att'
+python test_densenet.py --arch 'resnext101_32x8d' --img_size 256 --crop_size 224 --batch_size 64 --learning_rate 0.001 --gpu 0 --test_labels 'att'
 
 exit 0
