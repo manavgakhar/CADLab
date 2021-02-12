a2ad1aefaeb270df28a89c522276f5f0d7b1f380
Manav Gakhar <55736716+manavgakhar@users.noreply.github.com>
2021-02-12 14:44:47 +0530

    Delete panreas_hnn directory

--- panreas_hnn/matlab/dicom2volume.m
+++
@@ -1,8 +0,0 @@
-function dicom2volume(dicom2volume_exe,dicom_dir,output_file)    
-    
-    
-    mkdir(fileparts(output_file));
-    [status, result] = dos([dicom2volume_exe,' ',dicom_dir,' ',output_file],'-echo');
-    if status~=0
-        error(result)
-    end
