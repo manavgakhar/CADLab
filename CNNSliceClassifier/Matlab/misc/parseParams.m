5b41352e4e7a63e462b742519fea8b1d4e04a6b5
Manav Gakhar <55736716+manavgakhar@users.noreply.github.com>
2021-02-12 14:40:46 +0530

    Delete CNNSliceClassifier directory

--- CNNSliceClassifier/Matlab/misc/parseParams.m
+++
@@ -1,6 +0,0 @@
-% parseParams (script)
-
-    names = fieldnames(opt);
-    for i = 1:numel(names)
-        eval([names{i},' = opt.',names{i}]);
-    end
--- CNNSliceClassifier/my-cuda-convnet2/liver/run_predict_multiview_liver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PATIENT_START=1002
-PATIENT_END=1018
-
-for ((x=$PATIENT_START; x<=$PATIENT_END; x++))
-{
-   echo "......................... PREDICTING PATIENT $x ....................................."
-   ./liver/predict_multiview_liver.sh $x
-}
