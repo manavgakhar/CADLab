728991e45f3e18577fdf7468e34e58df719449f3
Manav Gakhar <55736716+manavgakhar@users.noreply.github.com>
2021-02-12 15:29:28 +0530

    Update run_train_CXR.sh

--- CXR-Binary-Classifier/run_train_CXR.sh
+++ CXR-Binary-Classifier/run_train_CXR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python train_densenet.py --img_size 256 --crop_size 224 --batch_size 32 --learning_rate 0.001 --gpu 0 --pretrained 'True'
+python train_densenet.py --arch 'resnext101_32x8d' --img_size 256 --crop_size 224 --batch_size 32 --learning_rate 0.001 --gpu 0 --pretrained 'True'
 
 exit 0
